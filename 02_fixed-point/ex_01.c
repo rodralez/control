ab0b15ec601bcd619c035153a78e2f925e3027de
rodralez <rodralez@gmail.com>
2018-03-13 11:28:30 -0300

    2018-03-13

--- 02_fixed-point/ex_01.c
+++ 02_fixed-point/ex_01.c
@@ -1,9 +1,10 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdint.h>
 
 void main(void)
 {	
-	signed char a, b, c, d, f;
+	signed char a, b, c, d, s1, s2;
 	
 	a = 127;
 	b = 127;
@@ -11,9 +12,14 @@ void main(void)
 	c = a + b;	
 	d = a * b;
 	
-	f = (-8) >> 1;
+	d = a * b;
+
+	s1 = (-8) >> 2;
+	s2 = (-1) >> 5;
 
 	printf("c = %d \n", c );
 	printf("d = %d \n", d );
-	printf("f = %d \n", f );
+	printf("s1 = %d \n", s1 );
+	printf("s2 = %d \n", s2 );
+
 }
