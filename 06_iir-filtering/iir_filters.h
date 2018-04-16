cdea69c04d2a8fc7c631a4cc3f82b0537b7280fc
rodralez <rodralez@gmail.com>
2018-04-16 10:29:05 -0300

    2018-04-16

--- 06_iir-filtering/iir_filters.h
+++ 06_iir-filtering/iir_filters.h
@@ -14,11 +14,11 @@
 #ifndef IIR_FILTERS_H
 #define IIR_FILTERS_H
 
-float second_order_IIR_direct_form_I_float  (const float input, const float * coeff, const float * gain);
-float second_order_IIR_direct_form_II_float (const float input, const float * coeff, const float * gain);
-
 void iir_filter_I_float (const float * input, const float * coeff,  const float * gain, const unsigned int N, float * output);
 void iir_filter_II_float(const float * input, const float * coeff,  const float * gain, const unsigned int N, float * output);
+
+float second_order_IIR_direct_form_I_float  (const float input, const float * coeff, const float * gain);
+float second_order_IIR_direct_form_II_float (const float input, const float * coeff, const float * gain);
 
 signed int second_order_IIR_direct_form_I_fixed(const signed int * coefficients, signed int input);
 signed int second_order_IIR_direct_form_II_fixed ( const signed int * coefficients, signed int input);
