2632fb879c921caf60ab57d8d0cba24398160728
rodralez <rodralez@gmail.com>
2019-03-19 11:36:11 -0300

    2019-03-18

--- 02_fixed-point/ex_01.c
+++ 02_fixed-point/ex_01.c
@@ -5,15 +5,13 @@
 void main(void)
 {	
 	signed char a, b, c, d, s1, s2;
-	
+	 
 	a = 127;
 	b = 127;
 	
 	c = a + b;	
 	d = a * b;
 	
-	d = a * b;
-
 	s1 = (-8) >> 2;
 	s2 = (-1) >> 5;
 
@@ -21,5 +19,4 @@ void main(void)
 	printf("d = %d \n", d );
 	printf("s1 = %d \n", s1 );
 	printf("s2 = %d \n", s2 );
-
 }
