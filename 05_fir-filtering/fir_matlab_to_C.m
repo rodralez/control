3b2143c19200862c224168c81e59dedb62568eb7
rodralez <rodralez@gmail.com>
2019-04-16 17:34:28 -0300

    2019-04-16

--- 05_fir-filtering/fir_matlab_to_C.m
+++ 05_fir-filtering/fir_matlab_to_C.m
@@ -1,4 +1,4 @@
-% fir_matlab_to_C: script to compare FIR filters implemented in C and MATLAB 
+% fir_matlab_to_C: script to compare FIR filters implemented in C and MATLAB
 % languages.
 %
 % Version: 002
@@ -12,37 +12,50 @@ clear
 
 %% PARAMETERS
 
-Fn1 = 500;   % Signal 1 frequency
-Fn2 = 60;    % Signal 2 frequency
+Fn1 = 200;   % Signal 1 frequency
+Fn2 = 600;   % Signal 2 frequency
+Fn3 = 50 ;   % Signal 3 frequency, interference
 Fs = 10000;  % Sampling frequency
 
 %%
 
 dt = 1/Fs;
-t = (0:dt:pi)';
+t = (0:dt:dt*2000)';
 
-signal = sin(2 * pi * Fn1 * t) + 0.5 * sin(2 * pi * Fn2 * t); 
+signal = sin(2 * pi * Fn1 * t) + sin(2 * pi * Fn2 * t) + 0.5 * sin(2 * pi * Fn3 * t);
 
 %% MATLAB-to-C, SINGLE PRECISION
 
 % Input arguments must be in float
 
-input_f = single(signal);
+signal_f = single(signal);
 
-output_c = fir_matlab_wrapper( input_f );
+output_c = fir_matlab_wrapper( signal_f );
 
 %% FIR MATLAB FILTERING, DOUBLE PRECISION
 
-
-%% PLOT
-
-N = 1000;
+%% PLOT IN TIME
 
 figure
 subplot(2,1,1)
-plot(t(1:N), input_f(1:N), '-b')
+plot(t, signal_f, '-')
 legend('ORIGINAL SIGNAL')
 subplot(2,1,2)
-plot(t(1:N), output_c(1:N), '-g')
+plot(t, output_c, '-')
 legend('FILTERED SIGNAL')
+
+%% PLOT IN FREQ
+
+[~, f1, ~, ~, H_signal_f, ~, ~] = my_dft(signal_f, Fs);
+[~, f2, ~, ~, H_output_c, ~, ~] = my_dft(output_c, Fs);
+
+figure
+subplot(2,1,1)
+plot(f1, H_signal_f, '-')
+legend('ORIGINAL SIGNAL')
+subplot(2,1,2)
+plot(f2, H_output_c, '-')
+legend('FILTERED SIGNAL')
+
+
 
