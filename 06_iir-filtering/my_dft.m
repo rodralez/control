8850f332b8fff676bd29f3db704df5f2b2509e5b
rodralez <rodralez@gmail.com>
2019-05-14 10:10:53 -0300

    2019-05-14

--- 06_iir-filtering/my_dft.m
+++ 06_iir-filtering/my_dft.m
@@ -4,7 +4,7 @@ function [f, dft_mag, dft_phase, dft, NFFT] = my_dft(data, Fs)
 % Si la unidad de data es U, la unidad de la TDF entregada es U/Hz.
 %
 % INPUT
-%   data: NxD serie de tiempo a analizar.
+%   data: NxD serie de tiempo.
 %   Fs: 1x1 frecuencia de muestreo de data.
 %
 % OUTPUT
@@ -21,9 +21,9 @@ function [f, dft_mag, dft_phase, dft, NFFT] = my_dft(data, Fs)
 [n, m] = size(data);
 
 if (m > n)							% Si data es un vector fila...
-	
-	data = data'; 					% Trasponer data	
-     n = m;
+    
+    data = data'; 					% Trasponer data
+    n = m;
 end
 
 L = n;                              % Tamaño de la serie de tiempo
@@ -31,15 +31,15 @@ L = n;                              % Tamaño de la serie de tiempo
 NFFT = 2^nextpow2(L);               % Potencia de 2 siguiente al tamaño del vector
 f = Fs/2*linspace(0,1,NFFT/2+1);    % Vector frecuencia
 
-dft = fft(data, NFFT) / L;          % Transformada rápida de Fourier
+dft = fft(data, NFFT) / L;          % Transformada rapida de Fourier
 
-dft = dft(1:NFFT/2+1, :); % Se toman solo las frecuencias positivas
+dft = dft(1:NFFT/2+1, :);           % Se toman solo las frecuencias positivas
+
+dft_mag = abs(dft);                 % Magnitud
+dft_phase = angle(dft);             % Fase
 
 % dft_r = real(dft);  % Parte real de la transformada discreta de Fourier
 % dft_i = imag(dft);  % Parte imaginaria de la transformada discreta de Fourier
-
-dft_mag = abs(dft);	% Magnitud
-dft_phase = angle(dft(1:NFFT/2+1, :));	% Fase
 
 end
 
