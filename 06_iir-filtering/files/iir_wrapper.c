ecdffecaeb0d6a79fd580b6c35128fba21500dbc
Rodrigo <rodralez@gmail.com>
2022-04-25 13:07:55 -0300

    2022-04-25

--- 06_iir-filtering/files/iir_wrapper.c
+++ 06_iir-filtering/files/iir_wrapper.c
@@ -1,35 +1,31 @@
 /* ========================================================================
- * fir_matlab_wrapper.c: interface MATLAB-TO-C functions for FIR
+ * iir_wrapper.c.c: interface MATLAB-TO-C functions for 2nd order
  * filtering.
  *
  * INPUT
  *      input  : NX1 input vector.
+ *      coeff  : 6x1 2nd-order IIR coefficients vector. 
+ *      gain   : 2X1 2nd-order IIR gains vector. 
  *
  *  OUTPUT
  *      output : NX1 output (filtered) vector.
  *
  * Version: 001
- * Date:    2017/04/03
+ * Date:    2021/10/28
  * Author:  Rodrigo Gonzalez <rodralez@frm.utn.edu.ar>
  * URL:     https://github.com/rodralez/control
  *
- * compile: mex fir_matlab_wrapper.c fir_filter.c
  * ===================================================================== */
 
-#include <stdio.h>
 #include "mex.h"
-#include "fir_filter.h"
-
+#include "iir_filter.h"
 
 /* The gateway function */
 void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[])
 {
-/*const float * input, const float * coefficients, const unsigned int N, float * output) */
-
 	float * input;	
 	float * output;
-	size_t N;
 	
     /* check for proper number of arguments */
     if(nrhs != 1) {
@@ -39,17 +35,10 @@ void mexFunction( int nlhs, mxArray *plhs[],
         mexErrMsgIdAndTxt("MyToolbox:iir_matlab_wrapper:nlhs","One output required.");
     }
     
-    /* make sure the first input argument is vector */
-    if( !mxIsSingle(prhs[0]) || 
-         mxIsComplex(prhs[0]) ||
-         mxGetNumberOfElements(prhs[0])==1 ) {
-        mexErrMsgIdAndTxt("MyToolbox:iir_matlab_wrapper:notScalar","Input multiplier must be a vector.");
-    }
-    
     /* make sure the first input argument is type single */
     if( !mxIsSingle(prhs[0]) || 
          mxIsComplex(prhs[0])) {
-        mexErrMsgIdAndTxt("MyToolbox:iir_matlab_wrapper:notSingle","Input matrix must be type single.");
+        mexErrMsgIdAndTxt("MyToolbo2x:iir_matlab_wrapper:notSingle","Input matrix must be type single.");
     }
     
     /* check that number of columns in input matrix argument is 1 */
@@ -60,9 +49,6 @@ void mexFunction( int nlhs, mxArray *plhs[],
     /* create a pointer to the real data in input matrix  */
     input = mxGetData(prhs[0]);
 
-    /* get number of rows in input matrix */
-    N = mxGetM(prhs[0]);
-
     /* create the output matrix */
     plhs[0] = mxCreateNumericArray(mxGetNumberOfDimensions(prhs[0]), mxGetDimensions(prhs[0]),
         mxSINGLE_CLASS, (mxIsComplex(prhs[0]) ? mxCOMPLEX : mxREAL));
@@ -71,7 +57,10 @@ void mexFunction( int nlhs, mxArray *plhs[],
     output = mxGetData(plhs[0]);
 
     /* call the computational C routine */
-    fir_filter_float(input, N, output); 
-    // fir_filter_fixed(input, N, output);   
-    
- }
+    iir_2nd_df1_float(input, output);
+//     iir_2nd_df2_float(input, output);
+//     iir_nth_df1_float(input, output);
+//     iir_nth_df2_float(input, output);
+     
+
+}
